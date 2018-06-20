aff51ad419f3ba854fb0cd180bd5387685396b4b
Nedyalko Nikolov <nedyalko.nikolov@softwaregroup.com>
2018-06-20 10:33:37 +0300

    Create symbolic links

---
+++ create_symbolic_links.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ln -s $(pwd)/.gitconfig ~/.gitconfig
+ln -s $(pwd)/.bash_aliases ~/.bash_aliases
+ln -s $(pwd)/.bash_functions ~/.bash_functions
+ln -s $(pwd)/.bash_profile ~/.bash_profile
+ln -s $(pwd)/.bashrc ~/.bashrc
+ln -s $(pwd)/.bash_variables ~/.bash_variables
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.zshrc ~/.zshrc
+ln -s $(pwd)/update.sh ~/update.sh
+ln -s $(pwd)/tmuxses.sh ~/tmuxses.sh
